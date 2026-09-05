c4a0ad83efcf9aa70e404af93d23b8c111dfc3e6
Evgeniy Pashko <ep.developer@yandex.com>
2026-09-05 22:41:05 +0700

    auth & token endpoint

---
+++ app/auth.py
@@ -0,0 +1,106 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.config import get_settings
+from app.db_depends import get_async_db
+from app.models.users import User as UserModel
+
+
+password_hash = PasswordHash.recommended()
+
+JWT_HASH_ALGORITHM: str = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
+
+
+def hash_password(password: str) -> str:
+    """
+    Преобразует пароль в безопасный хеш.
+    """
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Проверяет, соответствует ли введённый пароль сохранённому хешу.
+    """
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_secret_key() -> str:
+    """
+    Возвращает секретный ключ для JWT.
+    """
+    return get_settings().SECRET_KEY
+
+
+def create_access_token(data: dict) -> str:
+    """
+    Создаёт JWT-токен.
+    """
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        get_secret_key(),
+        algorithm=JWT_HASH_ALGORITHM,
+    )
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_async_db),
+):
+    """
+    Проверяет JWT и возвращает текущего пользователя из базы данных.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            get_secret_key(),
+            algorithms=[JWT_HASH_ALGORITHM],
+        )
+
+        email: str | None = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    stmt = select(UserModel).where(UserModel.email ==
+                                   email, UserModel.is_active == True)
+    result = await db.scalars(stmt)
+    user = result.first()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
